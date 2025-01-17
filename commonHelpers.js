30e2a913ebe61e4fcfccdf83d7e1276698a3a49b
margovit <163876014+margovit@users.noreply.github.com>
2025-01-17 18:59:55 +0000

    Deploying to gh-pages from @ margovit/periodictable2@c64ab3801438397ff938d9e963a51fa52f34a34c 🚀

---
+++ commonHelpers.js
@@ -0,0 +1,2 @@
+import{l as n}from"./assets/api-63bf6708.js";const a={form:document.querySelector(".js-form"),body:document.body};a.form.addEventListener("submit",async t=>{t.preventDefault();const o=new FormData(t.target),e=Object.fromEntries(o.entries());t.target.reset(),await n(e),window.location.pathname="/"});
+//# sourceMappingURL=commonHelpers.js.map
